d88e7b6f49905c91aab4cce961cbc21bdf4a7215
ASHUTOSH KUMAR <ashutoshfzl12@gmail.com>
2025-03-12 11:32:08 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,22 @@
+ const display = document.getElementById('display');
+  const buttons = document.querySelectorAll('button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', function() {
+      const value = this.getAttribute('data-value');
+
+      if (value === 'C') {
+        display.value = '';
+      } else if (value === '←') {
+        display.value = display.value.slice(0, -1);
+      } else if (value === '=') {
+        try {
+          display.value = eval(display.value);
+        } catch (error) {
+          display.value = 'Error';
+        }
+      } else {
+        display.value += value;
+      }
+    });
+  });
